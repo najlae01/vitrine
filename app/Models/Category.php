db7fb685eef4831e123e8018443bf0b63d8c11c0
najlae01 <88176530+najlae01@users.noreply.github.com>
2022-04-07 23:19:33 +0200

    Add files via upload

---
+++ app/Models/Category.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name_cat',
+        'description_cat',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
+
+    public function products()
+    {
+        return $this->hasMany('App\Models\Product');
+    }
+}
