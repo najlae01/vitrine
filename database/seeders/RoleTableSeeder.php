21fab22888d9d5be406af8bdfe50ac56cdd39d40
najlae01 <88176530+najlae01@users.noreply.github.com>
2022-04-07 23:24:41 +0200

    Add files via upload

---
+++ database/seeders/RoleTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Role;
+
+class RoleTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $role_employee = new Role();
+        $role_employee->name = 'internaute';
+        $role_employee->description = 'An internaute User';
+        $role_employee->save();
+        $role_manager = new Role();
+        $role_manager->name = 'admin';
+        $role_manager->description = 'An Admin User';
+        $role_manager->save();
+    }
+}
