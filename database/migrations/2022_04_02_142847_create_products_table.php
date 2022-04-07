0cf135551fe478a60476140782f555f707dc0bd4
najlae01 <88176530+najlae01@users.noreply.github.com>
2022-04-07 23:23:16 +0200

    Add files via upload

---
+++ database/migrations/2022_04_02_142847_create_products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->foreignId('category_id');
+            $table->string('code_pr');
+            $table->string('name_pr');
+            $table->string('description_pr');
+            $table->string('image');
+            $table->decimal('price');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
